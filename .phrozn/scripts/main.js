da879a29ef3fd7567e915c849c7e58deede5f640
aur1mas <aur1mas@devnet.lt>
2013-01-10 09:50:33 +0200

    changed past events layout

--- .phrozn/scripts/main.js
+++ .phrozn/scripts/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    $('body').delegate('.event-time', 'mouseover', function(){
-        $('.add-google-calendar').show();
-    });
-    $('body').delegate('.event-time', 'mouseout', function(){
-        $('.add-google-calendar').hide();
-    });
+    $('#myCarousel').carousel();
 });
