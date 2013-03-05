45eff25b39da4e07321cc1d6d8dc91a3f4aab259
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-03-05 14:43:14 +0200

    Removed public html.

--- public_html/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('body').delegate('.event-time', 'mouseover', function(){
-        $('.add-google-calendar').show();
-    });
-    $('body').delegate('.event-time', 'mouseout', function(){
-        $('.add-google-calendar').hide();
-    });
-});
